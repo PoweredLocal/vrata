283a07b5583fe2e44c553f936ab60276bd0d6ace
Denis Mysenko <denis@mysenko.com>
2016-08-23 11:45:00 +1000

    + First commit

---
+++ database/migrations/2016_08_22_013141_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('login', 64)->unique();
+            $table->string('display_name');
+            $table->string('email')->unique();
+            $table->string('password', 60);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->string('person', 256)->nullable();
+            $table->string('address', 256)->nullable();
+            $table->string('phone', 256)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
