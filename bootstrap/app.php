54561a85835a186fc981f6e02f5d9ef80e5dc3da
Denis Mysenko <denis@mysenko.com>
2016-10-05 13:37:24 +1100

    r CORS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -95,8 +95,8 @@ $app->routeMiddleware([
 $app->register(Appzcoder\LumenRoutesList\RoutesCommandServiceProvider::class);
 $app->register(Laravel\Passport\PassportServiceProvider::class);
 $app->register(Dusterio\LumenPassport\PassportServiceProvider::class);
-$app->register(App\Providers\AppServiceProvider::class);
 $app->register(\Barryvdh\Cors\LumenServiceProvider::class);
+$app->register(App\Providers\AppServiceProvider::class);
 //$app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'supportsCredentials' => false,
     'allowedOrigins' => ['*'],
-    'allowedHeaders' => ['Content-Type', 'Accept'],
+    'allowedHeaders' => ['Content-Type', 'Accept', 'Authorization'],
     'allowedMethods' => ['GET', 'POST', 'PUT', 'DELETE'],
     'exposedHeaders' => [],
     'maxAge' => 0,
