968358d3531344b1d7ac62b917a55b91085f91c8
dusterio <denis@mysenko.com>
2016-12-12 17:49:06 +1100

    f Log script

--- ci/logs-logentries.sh
+++ ci/logs-logentries.sh
@@ -6,7 +6,7 @@ apt-get -y update
 apt-get -y install logentries
 le reinit --user-key=${LOGGING_LOGENTRIES} --pull-server-side-config=False
 
-cat << EOF
+cat >> /etc/le/config << EOF
 [nginx]                                                                                                                                                                  
 path = /var/log/nginx/access.log                                                                                                                                         
 destination = ${LOGGING_ID}/nginx                                                                                                                                          
@@ -14,7 +14,7 @@ destination = ${LOGGING_ID}/nginx
 [app]                                                                                                                                                                    
 path = /home/app/storage/logs/lumen.log                                                                                                                                  
 destination = ${LOGGING_ID}/app       
-EOF >> /etc/le/config
+EOF
 
 apt-get -y install logentries-daemon
 service logentries start
