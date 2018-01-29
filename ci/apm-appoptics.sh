f96397548d81565c4958de7822e1deaae8b6df05
dusterio <denis@mysenko.com>
2018-01-29 15:44:21 +1100

    r Support for APM - App Optics

--- ci/apm-appoptics.sh
+++ ci/apm-appoptics.sh
@@ -2,7 +2,7 @@
 
 cp /root/appoptics.so /usr/lib/php/20151012/
 
-cat <<EOT > /etc/php/7.0/fpm/conf.d/25-appoptics.so
+cat <<EOT > /etc/php/7.0/fpm/conf.d/25-appoptics.ini
 extension=appoptics.so
 [appoptics]
 ; appoptics.service_key: unique service identifier
