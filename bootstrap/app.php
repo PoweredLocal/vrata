fa99fc895fdac714d510f47788988d4ffc9e2d6a
Denis Mysenko <denis@mysenko.com>
2019-07-16 14:35:25 +0700

    r deps update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -91,7 +91,6 @@ $app->middleware([
 */
 
 $app->register(Appzcoder\LumenRoutesList\RoutesCommandServiceProvider::class);
-$app->register(Laravel\Passport\PassportServiceProvider::class);
 $app->register(Dusterio\LumenPassport\PassportServiceProvider::class);
 $app->register(\Barryvdh\Cors\LumenServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
